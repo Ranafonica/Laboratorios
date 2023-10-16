067dfbb7ef0ddee4b2dcbe0df3f89cb0397b0076
Ranafonica <martin.bascunan.g@gmail.com>
2023-10-15 22:54:12 -0300

    Finalizacion de código

--- Proyecto1.c
+++ Proyecto1.c
@@ -8,7 +8,7 @@
 #define CARTAS_INICIALES 3
 #define CARTAS_EN_MAZO 15
 
-// Estructura de los Guardianes con sus características
+// Estructura para almacenar la información de los Guardianes.
 struct Guardian {
     char nombre[50];
     char tipo[50];
@@ -17,19 +17,19 @@ struct Guardian {
     int PD;
 };
 
-// Estructura para las cartas seleccionadas por un jugador
+// Estructura para almacenar la información de las cartas a utilizar.
 struct CartasSeleccionadas {
     struct Guardian guardianes;
     int barajaEnJuego;
 };
 
-// Estructura del jugador para el sistema de batalla
+// Estructura para almacenar la información del Jugador.
 struct Jugador {
     struct CartasSeleccionadas mano[CARTAS_INICIALES];
     int puntosVida;
 };
 
-// Declaración de funciones
+// Declaración de funciones y voids antes del Main().
 void MenuInicial();
 int CargarGuardianes(struct Guardian cartas[]);
 void CrearMazoAleatorio(struct Guardian cartas[], struct CartasSeleccionadas mazo[], int numCartas, int numCartasEnMazo);
@@ -57,7 +57,7 @@ int main() {
     computadora.puntosVida = 3;
 
     numCartas = CargarGuardianes(cartas);
- 
+ 	
     struct CartasSeleccionadas mazoJugador[CARTAS_EN_MAZO];
     struct CartasSeleccionadas mazoComputadora[CARTAS_EN_MAZO];
     struct CartasSeleccionadas manoJugador[CARTAS_INICIALES];
@@ -71,20 +71,29 @@ int main() {
 
         printf("\nSELECCIONE UNA OPCION: \n");
         scanf("%d", &opcion);
-        printf("HAS SELECCIONADO: ");
-
+		
+		//Bucle del juego dependiendo de la seleccion del jugador en el menú inicial
         switch (opcion) {
             case 1:
+            	printf("HAS SELECCIONADO: ");
                 printf("OPCION CREAR CARTA\n");
                 break;
             case 2:
-                printf("OPCION EMPEZAR JUEGO\n");
+            	printf("EMPEZANDO JUEGO");
+            	Sleep(1000);
+                printf(".");
+                Sleep(1000);
+                printf(".");
+                Sleep(1000);
+                printf(".");
+                Sleep(3000);
+                system("cls");
                 // Crea mazos aleatorios de 15 cartas cada uno, para el jugador y para la computadora respectivamente.
                 CrearMazoAleatorio(cartas, mazoJugador, numCartas, CARTAS_EN_MAZO);
                 CrearMazoAleatorio(cartas, mazoComputadora, numCartas, CARTAS_EN_MAZO);
 
                 // Mostrar el mazo del jugador
-                MostrarMazo(mazoJugador, CARTAS_EN_MAZO, "TU MAZO DE 15 CARTAS ES: \n");
+                MostrarMazo(mazoJugador, CARTAS_EN_MAZO, "TU MAZO DE 15 CARTAS ES: ");
 
                 // Seleccionar 3 cartas del mazo del jugador
                 SeleccionarCartasIniciales(mazoJugador, CARTAS_EN_MAZO, manoJugador, CARTAS_INICIALES);
@@ -92,9 +101,16 @@ int main() {
                 // Mostrar las cartas seleccionadas por el jugador
                 MostrarMazo(manoJugador, CARTAS_INICIALES, "Tus cartas seleccionadas:");
 
-                printf("ESPERE UN SEGUNDO...\n");
-                Sleep(5000);
-                printf("AHORA SE MOSTRARAN LAS ESCOGIDAS POR LA COMPUTADORA\n");
+                printf("BUSCANDO PARTIDA");
+                Sleep(1000);
+                printf(".");
+                Sleep(1000);
+                printf(".");
+                Sleep(1000);
+                printf(".");
+                printf(" !PARTIDA ENCONTRADA!");
+                Sleep(2000);
+                printf("\nAHORA SE MOSTRARAN LAS ESCOGIDAS POR LA COMPUTADORA\n");
                 Sleep(5000);
 
                 // Seleccionar 3 cartas del mazo de la computadora
@@ -104,7 +120,7 @@ int main() {
                 MostrarMazo(manoComputadora, CARTAS_INICIALES, "CARTAS DE LA COMPUTADORA:");
                 
                 Sleep(5000);
-                printf("LISTO PARA EMPEZAR LA BATALLA?...");
+                printf("MOVIENDO AL CAMPO DE BATALLA...");
                 Sleep(5000);
                 system ("cls");
                 
@@ -112,18 +128,21 @@ int main() {
                 BucleTurnos(cartas, manoJugador, manoComputadora, numCartas, &jugadorpuntosVida, &computadorapuntosVida);
                 break;
             case 3:
+            	printf("HAS SELECCIONADO: ");
                 printf("OPCION HISTORIAL DE PARTIDAS\n");
                 break;
             case 4:
+            	printf("HAS SELECCIONADO: ");
                 printf("OPCION CERRAR\n");
                 printf("GRACIAS POR JUGAR CLASH OF THE GUARDIANS :D\n");
                 break;
         }
-    } while (opcion <= 0 || opcion >= 5 || opcion !=4);
+    } while (opcion <= 0 || opcion >= 5);
 
     return 0;
 }
 
+// Funcion que permite cargar los Guardianes desde el archivo texto llamado "Guardians.txt", si este es alterado, saltará un mensaje de alerta.
 int CargarGuardianes(struct Guardian cartas[]) {
     FILE *archivo = fopen("Guardians.txt", "r");
     if (archivo == NULL) {
@@ -142,6 +161,7 @@ int CargarGuardianes(struct Guardian cartas[]) {
     return numCartas;
 }
 
+// Menú inicial que introduce al jugador al juego, le brinda una seleccion de opciones a escoger de manera explícita.
 void MenuInicial() {
     printf("--------------------[CLASH OF THE GUARDIANS]--------------------\n");
     printf("-----[OPCIONES]\n");
@@ -151,6 +171,7 @@ void MenuInicial() {
     printf("4-SALIR DEL JUEGO\n");
 }
 
+// Funcion que crea mazos de forma aleatoria, los cuales, del total de 60 guardianes, 15 serán para el jugador y otras 15 serán para la computadora.
 void CrearMazoAleatorio(struct Guardian cartas[], struct CartasSeleccionadas mazo[], int numCartas, int numCartasEnMazo) {
     int cartasDisponibles[MAX_CARTAS];
     int numCartasDisponibles = numCartas;
@@ -159,7 +180,7 @@ void CrearMazoAleatorio(struct Guardian cartas[], struct CartasSeleccionadas maz
         cartasDisponibles[i] = i;
     }
 
-    // Selección aleatoria de cartas para el mazo
+    // Selección aleatoria de cartas para el mazo.
     for (int i = 0; i < numCartasEnMazo; i++) {
         int cartaAleatoria = rand() % numCartasDisponibles;
         int eleccion = cartasDisponibles[cartaAleatoria];
@@ -174,6 +195,7 @@ void CrearMazoAleatorio(struct Guardian cartas[], struct CartasSeleccionadas maz
     }
 }
 
+// De la seleccion de las 15 cartas aleatorias, esta funcion será específicamente para que la computadora escoga de las 15 cartas, 3 para su baraja.
 void SeleccionarCartasAleatorias(struct CartasSeleccionadas mazo[], int numCartasEnMazo, struct CartasSeleccionadas baraja[], int numCartasIniciales) {
     for (int i = 0; i < numCartasIniciales; i++) {
         int cartaAleatoria;
@@ -185,6 +207,7 @@ void SeleccionarCartasAleatorias(struct CartasSeleccionadas mazo[], int numCarta
     }
 }
 
+// Funcion que permite al Jugador seleccionar entre las 15 cartas de su mazo, 3 a su propia eleccion, sabiendo antes su baraja escogida por el programa.
 void SeleccionarCartasIniciales(struct CartasSeleccionadas mazo[], int numCartasEnMazo, struct CartasSeleccionadas baraja[], int numCartasIniciales) {
     printf("SELECCIONA TUS CARTAS INICIALES (NUMEROS DEL 1 al %d):\n", numCartasEnMazo);
     for (int i = 0; i < numCartasIniciales; i++) {
@@ -200,6 +223,7 @@ void SeleccionarCartasIniciales(struct CartasSeleccionadas mazo[], int numCartas
     }
 }
 
+// Funcion que permite mostrar todas las cartas del mazo, incluyendo también las cartas nuevas. Aqui se mostrarán todas las cartas agregadas al archivo texto "Guardians.txt"
 void MostrarCartas(struct Guardian cartas[], int numCartas) {
     printf("\n[MAZO DE CARTAS OFICIAL]:\n");
     for (int i = 0; i < numCartas; i++) {
@@ -212,9 +236,12 @@ void MostrarCartas(struct Guardian cartas[], int numCartas) {
     }
 }
 
+// Esta funcion muestra el mazo de 3 cartas escogido, en pantalla.
 void MostrarMazo(struct CartasSeleccionadas baraja[], int numCartasIniciales, const char* mensaje) {
     printf("\n%s\n", mensaje);
     for (int i = 0; i < numCartasIniciales; i++) {
+    	Sleep(1000);
+    	printf("%d-", i+1);
         printf("Nombre: %s\n", baraja[i].guardianes.nombre);
         printf("Tipo: %s\n", baraja[i].guardianes.tipo);
         printf("PV: %d\n", baraja[i].guardianes.PV);
@@ -224,15 +251,19 @@ void MostrarMazo(struct CartasSeleccionadas baraja[], int numCartasIniciales, co
     }
 }
 
+// Genera el bucle del juego, el sistema de ataque y defensa, los puntos de vida y como se gestionan las cartas en cada partida, en cierta forma es el corazón del programa.
 void BucleTurnos(struct Guardian cartas[], struct CartasSeleccionadas manoJugador[], struct CartasSeleccionadas manoComputadora[], int numCartas, int *jugadorpuntosVida, int *computadorapuntosVida) {
     int turno = 1;
+    int cartabatallaJugador = 0; // Almacena la elección del jugador
+
     while (*jugadorpuntosVida > 0 && *computadorapuntosVida > 0) {
         printf("\n[ESTADO ACTUAL DE PARTIDA]\n");
+        
         printf("VIDAS JUGADOR: %d\n", *jugadorpuntosVida);
         printf("VIDAS COMPUTADORA: %d\n", *computadorapuntosVida);
-
+        
         if (turno == 1) {
-            printf("\n[TURNO DEL JUGADOR]\n");
+            printf("\n-----TURNO DEL JUGADOR-----\n");
             MostrarMazo(manoJugador, CARTAS_INICIALES, "Tus cartas seleccionadas:");
 
             int cartabatalla;
@@ -245,38 +276,43 @@ void BucleTurnos(struct Guardian cartas[], struct CartasSeleccionadas manoJugado
             if (dano <= 0) {
                 dano = 0;
             }
-            printf("\n%s ATACA A LA COMPUTADORA POR %d PUNTOS DE DANO\n", manoJugador[cartabatalla - 1].guardianes.nombre, dano);
+            printf("\n[%s] ATACA A [%s] POR %d PUNTOS DE DANO\n", manoJugador[cartabatalla - 1].guardianes.nombre, manoComputadora[0].guardianes.nombre, dano);
+            if (dano != 0) {
+                dano = 1;
+            }
             *computadorapuntosVida -= dano;
 
-            if (*computadorapuntosVida <= 0) {
+            if (dano != 0) {
                 printf("\n%s HA DERROTADO AL GUARDIAN DE LA COMPUTADORA\n", manoJugador[cartabatalla - 1].guardianes.nombre);
-                break;
             }
+            // Almacena la elección del jugador
+            cartabatallaJugador = cartabatalla;
         } else {
-        	printf("\nESPERE UN SEGUNDO...\n");
-            Sleep(5000);
-            printf("\n[TURNO DE LA COMPUTADORA]: \n");
-            int cartabatalla = rand() % 3;
-            int dano = manoComputadora[cartabatalla].guardianes.PA - manoJugador[0].guardianes.PD;
+        	Sleep(3000);
+            printf("\n-----TURNO DE LA COMPUTADORA-----\n");
+            int cartaComputadora = rand() % CARTAS_INICIALES;
+            int dano = manoComputadora[cartaComputadora].guardianes.PA - manoJugador[cartabatallaJugador - 1].guardianes.PD;
 
             if (dano <= 0) {
                 dano = 0;
             }
-            Sleep(5000);
-            printf("\n%s ATACA AL JUGADOR POR %d PUNTOS DE DANO!\n", manoComputadora[cartabatalla].guardianes.nombre, dano);
+            printf("\n[%s] ATACA A [%s] POR %d PUNTOS DE DANO!\n", manoComputadora[cartaComputadora].guardianes.nombre, manoJugador[cartabatallaJugador - 1].guardianes.nombre, dano);
+            if (dano != 0) {
+                dano = 1;
+            }
             *jugadorpuntosVida -= dano;
 
-            if (*jugadorpuntosVida <= 0) {
-                printf("%s HA DERROTADO AL GUARDIAN DEL JUGADOR\n", manoComputadora[cartabatalla].guardianes.nombre);
-                break;
+            if (dano != 0) {
+                printf("%s HA DERROTADO AL GUARDIAN DEL JUGADOR\n", manoComputadora[cartaComputadora].guardianes.nombre);
             }
         }
         turno = (turno == 1) ? 2 : 1;
     }
-    if (*jugadorpuntosVida = 0) {
+    if (*jugadorpuntosVida == 0) {
         printf("\nLA COMPUTADORA HA GANADO LA PARTIDA\n");
     } else {
-        printf("\nHAS GANADO LA PARTIDA\n");
+        printf("HAS GANADO LA PARTIDA\n");
     }
 }
+
 
