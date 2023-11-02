e04824e55d0bf22e1475b36cc43a224498f8e90e
Ranafonica <127953603+Ranafonica@users.noreply.github.com>
2023-11-01 22:22:57 -0300

    Actualizacion mejoras

--- Laboratorios U2/Laboratorio 07/Laboratorio 07.cpp
+++ Laboratorios U2/Laboratorio 07/Laboratorio 07.cpp
@@ -46,36 +46,64 @@ void imprimir(Nodo* root){
 	imprimir(root->right);
 }
 
-bool buscar(Nodo* root, int search){
+bool buscar(Nodo* root, int datos){
 	if (root == NULL){
-		cout << "null " << endl;
 		return false;
 	}
-	else if (root->datos == search){
-		cout << "S " << endl;
+	else if (root->datos == datos){		
 		return true;
 	}
-	else if (root->datos > search){
-		cout << "M " << endl;
-		return buscar (root->left, search);
-	}else{
-		cout << "AC " << endl;
-		return buscar (root->right, search);
+	else if (datos<= root -> datos){		
+		return buscar (root->left, datos);
+	}else{		
+		return buscar (root->right, datos);
 	}
+}
+
+Nodo* eliminarNodo(Nodo* root, int llave){
+	if (root = NULL){
+		return root;
+	}
+	if (llave < root->datos){
+		root->left = eliminarNodo(root->left, llave);
+		
+	}else if(llave > root->datos){
+		root->right = eliminarNodo(root->right, llave);
+	}else{
+		if (root->left == NULL){
+			Nodo* temp = root ->right;
+			delete root;
+			return temp;
+		} else if (root->right == NULL){
+			Nodo* temp = root->left;
+			delete root;
+			return temp;
+		}
+		
+		Nodo* temp = root->right;
+		while (temp->left != NULL){
+			temp = temp->left;
+		}
+		
+		root->datos = temp->datos;
+		root->right = eliminarNodo(root->right, temp->datos);
+	}
+	
+	return root;
 }
 
 int main (){
 	Nodo* root = NULL;
 	
-	int ingreso, totalnum, search, opcion;
+	int ingreso, totalnum, search, eliminar, opcion, llave=0;
 	
 	cout << "Laboratorio 07-Arboles Binarios";
 	cout << "\n" << endl;
 	cout << "Porfavor Ingrese la opcion a escoger:" << endl;
 	cout << "1-Agregar Numero Positivo" << endl;
-	cout << "2-Buscar Numero Especifico Minimo y Maximo" << endl;
+	cout << "2-Buscar Numero Especifico, Minimo y Maximo" << endl;
 	cout << "3-Borrar Numero de la Raiz" << endl;
-	cout << "4-Cerrar el programa" << endl;
+	cout << "4-Implementar Arbol General" << endl;
 	cin >> opcion;
 	switch(opcion){
 		case 1:{
@@ -92,7 +120,40 @@ int main (){
 			break;
 		}
 		case 2:{
+			root = agregar(root, 23); // Valores de prueba para la búsqueda de numeros.
+			root = agregar(root, 15); // Valores de prueba para la búsqueda de numeros.
 			
+			cout << "Ingrese valor a buscar: " << endl;
+			cin >> search;
+			
+			if(buscar(root, search)==true){
+				cout << "VALOR ENCONTRADO";
+			
+			}
+			else if(buscar(root, search)==false){
+				cout << "VALOR NO ENCONTRADO";
+			
+			}
+			
+			break;
+		}
+		case 3:{
+			root = agregar(root, 23); // Valores de prueba para la búsqueda de numeros.
+			root = agregar(root, 15); // Valores de prueba para la búsqueda de numeros.
+			
+			cout << "Ingrese el valor a eliminar: " << endl;
+			cin >> eliminar;
+			llave = eliminar;
+			
+			if (buscar(root, eliminar)== true){
+				cout << "NUMERO ENCONTRADO" <<endl;
+				
+				eliminarNodo(root, llave);
+				cout << "NUMERO ELIMINADO";
+				
+			}else if(buscar(root,eliminar)== false){
+				cout << "NUMERO NO ENCONTRADO" <<endl;
+			}
 			break;
 		}			
 	}
